6248a1ed979d672d7ec9c45241abf3ca7b83c482
andrerie (Andrea Riebler) <andrea.riebler@math.ntnu.no>
2022-03-23 14:00:54 +0000

    updated control.compute statement to get marginals for linear predictor
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bapc@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/BAPC.r
+++ pkg/R/BAPC.r
@@ -134,9 +134,10 @@ BAPC <- function(APCList,  predict = list(npredict = 0, retro = TRUE),
       control.predictor=list(compute=TRUE, link=1),
       lincomb=lincomb,
       #quantiles=c(0.025, 0.1, 0.25, 0.5, 0.75, 0.9, 0.975),
-      control.compute=list(config=config, dic=TRUE, cpo=TRUE),
-      #control.inla=list(lincomb.derived.only=TRUE, 
-      #                  lincomb.derived.correlation.matrix=TRUE), 
+      control.compute=list(config=config, dic=TRUE, cpo=TRUE,
+                           return.marginals.predictor=TRUE),
+      control.inla=list(#lincomb.derived.only=TRUE, 
+                        lincomb.derived.correlation.matrix=TRUE), 
       verbose=verbose)
   inlares(APCList) <- res
   
